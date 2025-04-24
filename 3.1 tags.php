cb26b43dc3f2ae821e6889e2ebe8ab8f118ee489
Hung <Hung@gmail.com>
2025-04-24 09:06:32 +0700

    222

--- 3.1 tags.php
+++ 3.1 tags.php
@@ -8,7 +8,7 @@
                 color: red;
             }
         </style>
-        <h1>Xin chào, tôi là <?php echo "Lưu Quang Hùng"; ?></h1>
+        <h1>Xin chào, tôi là <?php echo "Lưu Quang H"; ?></h1>
     </body>
 </html>
 
